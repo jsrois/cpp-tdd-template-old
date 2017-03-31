2e5e0cb61c85305d006c83d46cfb944aa840761f
Javier Sánchez Rois <jsanchez@gradiant.org>
2017-03-31 17:07:32 +0200

    Added catch+fakeit support

--- src/elevator.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Javier Sánchez Rois on 17/01/17.
-//
-
-#include "elevator.h"
---
+++ test/handler.test.cpp.cpp
@@ -0,0 +1,31 @@
+//
+// Created by jsanchez on 31/03/17.
+//
+
+
+#include <catch.hpp>
+#include <fakeit.hpp>
+using namespace fakeit;
+
+
+typedef std::string Request;
+
+struct RequestProcessor {
+  // if not virtual: "DynamicProxy requires a polymorphic type"
+  virtual ~RequestProcessor() = default;
+  virtual void processRequest(Request) {}
+};
+
+struct Handler {
+  Handler(RequestProcessor * p) { processor = p;}
+  void handle(Request request) { processor->processRequest(request);}
+  RequestProcessor * processor;
+};
+
+TEST_CASE("Handler processes incoming requests", "[mock]") {
+  Mock<RequestProcessor> processor;
+  Fake(Method(processor,processRequest));
+  Handler handler(&(processor.get()));
+  Request sampleRequest("requestContent");
+  Verify(Method(processor,processRequest).Using(sampleRequest)).Exactly(1_Times);
+}
